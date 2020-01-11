8e8befdc29ed7fdd591e4dae926f356af4784e03
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-01-11 01:37:01 -0700

    first commit

---
+++ open_tex.py
@@ -0,0 +1,33 @@
+"""
+A script to open a tex file in i3
+- Opens the corresponding pdf
+- Opens a terminal to watch the file
+- Opens the tex file in vim
+"""
+import sys
+import tempfile
+import os
+from subprocess import call
+from i3ipc import Connection, Event
+import argparse
+import subprocess
+import time
+
+parser = argparse.ArgumentParser(description='Tex file opener')
+
+parser.add_argument('file', metavar='NAME', help='Latex file name to open')
+
+
+args = parser.parse_args()
+i3 = Connection()
+i3.command('split h')
+subprocess.Popen(["alacritty", "--working-directory="+os.getcwd(), "-e", "zsh", "-i", "-c","nvim "+args.file+".tex"])
+time.sleep(0.2)
+subprocess.Popen(["zathura", args.file+".pdf"])
+time.sleep(0.2)
+i3.command('focus right, focus right, split v')
+time.sleep(0.2)
+subprocess.Popen(["alacritty", "--working-directory="+os.getcwd(), "-e", "zsh", "-i", "-c","ls *.tex | entr -s 'latexmk -pdf' "])
+i3.command('focus down')
+time.sleep(0.2)
+i3.command('resize shrink height 40 px or 40 ppt')
